1da29248ca9b5369cafe228a90c7bf889219b937
Miron Lawniczak <m.lawniczak@tkhtechnology.com>
2022-11-14 12:32:30 +0100

    masz zadanko mlody

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Fiszki from "./pages/Fiszki";
 import DarkContext from "./context/dark-context";
 import Toogle from "./components/Toogle";
 import { Link, Route, Switch } from "react-router-dom";
-import Quiz from './pages/Quiz'
-import Main from './pages/Main'
-import Login from "./pages/Login"
-import User from "./pages/User"
-import Pisz from "./pages/Pisz"
+import Quiz from "./pages/Quiz";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import User from "./pages/User";
+import Pisz from "./pages/Pisz";
+import axios from "axios";
 
 function App() {
+  // zrob to tak z komponentem loginu i inputami
+  // aby data bylo dynamiczne, tzn dajesz tam useState
+  // i potem na klickniecie przycisku ma wykonax axiosa
+
+  // const data = {
+  //   email: "karol@wp.pl",
+  //   password: "qwerty",
+  // };
+
+  // useEffect(() => {
+  //   axios
+  //     .post("http://www.localhost:5000/login", data)
+  //     .then((respose) => console.log(respose));
+  // }, []);
+
   let isdark = localStorage.getItem("isdark");
   isdark = JSON.parse(localStorage.getItem("isdark"));
 
@@ -19,11 +35,11 @@ function App() {
   return (
     <DarkContext.Provider value={value}>
       <Switch>
-      <Route exact path="/">
-          <Main/>
+        <Route exact path="/">
+          <Main />
         </Route>
         <Route path="/quiz">
-          <Quiz/>
+          <Quiz />
         </Route>
         <Route path="/fiszka">
           <Fiszki />
